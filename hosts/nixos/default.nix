2da20b08e844a7a7c775b547c9218b5fa26a9038
caldetas <misterhannes@hotmail.com>
2025-05-13 13:36:48 +0200

    server init

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -10,7 +10,6 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./nvidia.nix
       ../../modules/desktops/virtualisation/docker.nix
     ] ++
     (import ../../modules/desktops ++
