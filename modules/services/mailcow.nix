adb3df80f7fe266a3bbc5c0e14588a0025a25ce0
caldetas <misterhannes@hotmail.com>
2025-05-14 13:33:45 +0200

    mailcow

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -35,22 +35,22 @@ with lib;
           '';
         };
 
-        locations."/SOGo/" = {
-          proxyPass = "http://127.0.0.1:8088/SOGo/";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header x-webobjects-server-protocol HTTP/1.0;
-            proxy_set_header x-webobjects-remote-host 127.0.0.1;
-            proxy_set_header x-webobjects-server-name $host;
-            proxy_set_header x-webobjects-server-port $server_port;
-            proxy_set_header x-webobjects-server-url $scheme://$host;
-            proxy_http_version 1.1;
-          '';
-        };
+        #        locations."/SOGo/" = {
+        #          proxyPass = "http://127.0.0.1:8088/SOGo/";
+        #          proxyWebsockets = true;
+        #          extraConfig = ''
+        #            proxy_set_header Host $host;
+        #            proxy_set_header X-Real-IP $remote_addr;
+        #            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        #            proxy_set_header X-Forwarded-Proto $scheme;
+        #            proxy_set_header x-webobjects-server-protocol HTTP/1.0;
+        #            proxy_set_header x-webobjects-remote-host 127.0.0.1;
+        #            proxy_set_header x-webobjects-server-name $host;
+        #            proxy_set_header x-webobjects-server-port $server_port;
+        #            proxy_set_header x-webobjects-server-url $scheme://$host;
+        #            proxy_http_version 1.1;
+        #          '';
+        #        };
       };
     };
   };
