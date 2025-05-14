2cf6d82131f61a7ee39e50e4d1b9c9f76a8f9b33
caldetas <misterhannes@hotmail.com>
2025-05-14 13:41:21 +0200

    mailcow

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -19,7 +19,7 @@ with lib;
     services.nginx = {
       enable = true;
 
-      virtualHosts."mail.${vars.domain}" = {
+      virtualHosts."webmail.${vars.domain}" = {
         forceSSL = pkgs.lib.strings.hasInfix "." vars.domain;
         enableACME = pkgs.lib.strings.hasInfix "." vars.domain;
 
@@ -34,23 +34,6 @@ with lib;
             proxy_set_header X-Forwarded-Proto $scheme;
           '';
         };
-
-        #        locations."/SOGo/" = {
-        #          proxyPass = "http://127.0.0.1:8088/SOGo/";
-        #          proxyWebsockets = true;
-        #          extraConfig = ''
-        #            proxy_set_header Host $host;
-        #            proxy_set_header X-Real-IP $remote_addr;
-        #            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        #            proxy_set_header X-Forwarded-Proto $scheme;
-        #            proxy_set_header x-webobjects-server-protocol HTTP/1.0;
-        #            proxy_set_header x-webobjects-remote-host 127.0.0.1;
-        #            proxy_set_header x-webobjects-server-name $host;
-        #            proxy_set_header x-webobjects-server-port $server_port;
-        #            proxy_set_header x-webobjects-server-url $scheme://$host;
-        #            proxy_http_version 1.1;
-        #          '';
-        #        };
       };
     };
   };
