670be9c1b526bcc35240be7df3d484ae917ed0d8
caldetas <misterhannes@hotmail.com>
2025-05-14 13:08:46 +0200

    mailcow

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -15,25 +15,26 @@ with lib;
     };
   };
 
+  config = mkIf (config.bitwarden.enable) {
+    services.nginx = {
+      enable = true;
 
-  services.nginx = {
-    enable = true;
+      virtualHosts."mail.${vars.domain}" = {
+        forceSSL = pkgs.lib.strings.hasInfix "." vars.domain;
+        enableACME = pkgs.lib.strings.hasInfix "." vars.domain;
 
-    virtualHosts."mail.${vars.domain}" = {
-      forceSSL = pkgs.lib.strings.hasInfix "." vars.domain;
-      enableACME = pkgs.lib.strings.hasInfix "." vars.domain;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8088";
+          proxyWebsockets = true;
 
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8088";
-        proxyWebsockets = true;
-
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_http_version 1.1;
-        '';
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_http_version 1.1;
+          '';
+        };
       };
     };
   };
