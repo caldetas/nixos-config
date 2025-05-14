04fa2185b98aedec9e5675fb5107be6d0a376f4b
caldetas <misterhannes@hotmail.com>
2025-05-14 16:30:42 +0200

    cz

--- hosts/nixcz/default.nix
+++ hosts/nixcz/default.nix
@@ -86,6 +86,8 @@
   # This is the server's hostname you chose during the order process. Feel free to change it.
 
   networking.hostName = "nixcz";
+  services.getty.enable = true;
+  services.getty.autoLogin.user = "${vars.user};
 
   # We use the dhcpcd daemon to automatically configure your network. For IPv6 we need to make sure
   # that no temporary addresses (or privacy extensions) are used. Your server is required to use the
@@ -94,29 +96,33 @@
 
   #  networking.dhcpcd.IPv6rs = true;
   #  networking.dhcpcd.persistent = true;
-  #  networking.tempAddresses = "disabled";
-  #  networking.interfaces.ens3.tempAddress = "disabled";
+  #  networking.tempAddresses = " disabled ";
+    #  networking.interfaces.ens3.tempAddress = "
+    disabled ";
 
   # To allow you to properly use and access your VPS via SSH, we enable the OpenSSH server and
   # grant you root access. This is just our default configuration, you are free to remove root
   # access, create your own users and further secure your server.
 
   services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
-  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+  services.openssh.settings.PermitRootLogin = "
+    yes ";
+    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
 
-  # Under normal circumstances we would listen to your server's cloud-init callback and mark the server
-  # as installed at this point. As we don't deliver cloud-init with NixOS we have to use a workaround
-  # to indicate that your server is successfully installed. You can remove the cronjob after the server
-  # has been started the first time. It's no longer needed.
+    # Under normal circumstances we would listen to your server's cloud-init callback and mark the server
+    # as installed at this point. As we don't deliver cloud-init with NixOS we have to use a workaround
+    # to indicate that your server is successfully installed. You can remove the cronjob after the server
+    # has been started the first time. It's no longer needed.
 
-  services.cron.enable = true;
+    services.cron.enable = true;
 
-  # Please remove the hardcoded password from the configuration and set
-  # the password using the "passwd" command after the first boot.
+    # Please remove the hardcoded password from the configuration and set
+    # the password using the "
+    passwd " command after the first boot.
 
   #  bitwarden.enable = true;
   #  mailcow.enable = true;
 
 }
+
 
