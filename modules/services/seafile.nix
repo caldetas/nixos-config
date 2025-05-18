07f0ac7ddbd483c89afb06fe725793f46c40f93e
caldetas <misterhannes@hotmail.com>
2025-05-18 11:06:52 +0200

    seafile

--- modules/services/seafile.nix
+++ modules/services/seafile.nix
@@ -117,15 +117,14 @@ with lib;
           };
           "/seafhttp/" = {
             proxyPass = "http://127.0.0.1:8000/seafhttp/";
-            proxySetHeaders = {
-              Host = "$host";
-              X-Real-IP = "$remote_addr";
-              X-Forwarded-For = "$proxy_add_x_forwarded_for";
-              X-Forwarded-Host = "$server_name";
-            };
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Host $server_name;
+            '';
           };
         };
-
       };
     };
 
