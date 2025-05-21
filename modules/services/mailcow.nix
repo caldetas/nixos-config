0f14392b752a6268428f12891cd9010d069ec407
caldetas <misterhannes@hotmail.com>
2025-05-21 13:12:45 +0200

    seafile

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -54,7 +54,7 @@ with lib;
           proxyWebsockets = true;
 
           extraConfig = ''
-            proxy_set_header Host mail.caldetas.com;
+            proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto $scheme;
