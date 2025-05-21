95d3a6983d1812409dda1962d6902d9409e76b8e
caldetas <misterhannes@hotmail.com>
2025-05-21 13:59:10 +0200

    seafile

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -16,23 +16,6 @@ with lib;
   };
 
   config = mkIf (config.mailcow.enable) {
-
-    #    systemd.services.mailcow = {
-    #      description = "Mailcow Docker Compose Service";
-    #      after = [ "docker.service" "network.target" ];
-    #      wants = [ "docker.service" ];
-    #      wantedBy = [ "multi-user.target" ];
-    #
-    #      serviceConfig = {
-    #        WorkingDirectory = "/home/${vars.user}/git/mailcow-dockerized";
-    #        ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
-    #        ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
-    #        Restart = "always";
-    #        RestartSec = 10;
-    #        User = "${vars.user}";
-    #      };
-    #    };
-
     services.nginx = {
       virtualHosts."mail.${vars.domain}" = {
         forceSSL = pkgs.lib.strings.hasInfix "." vars.domain; # Use SSL only for real domain
@@ -40,13 +23,6 @@ with lib;
         locations."/" = {
           proxyPass = "http://127.0.0.1:8088";
           proxyWebsockets = true;
-
-          #          extraConfig = ''
-          #            proxy_set_header Host $host;
-          #            proxy_set_header X-Real-IP $remote_addr;
-          #            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          #            proxy_set_header X-Forwarded-Proto https;
-          #          '';
         };
       };
     };
