22228adbef9e77f128ee733f264dd7f1328191f5
caldetas <misterhannes@hotmail.com>
2025-05-21 10:42:02 +0200

    seafile

--- modules/services/seafile2.nix
+++ modules/services/seafile2.nix
@@ -26,12 +26,10 @@ with lib;
           web_token_expire_time = 36000;
         };
       };
-      seahubSettings = {
-        "ALLOWED_HOSTS" = "['seafile.${vars.domain}']";
-      };
       seahubExtraConf = ''
         CSRF_TRUSTED_ORIGINS = ["https://seafile.${vars.domain}"]
         FILE_SERVER_ROOT =  "https://seafile.${vars.domain}/seafhttp"
+        ALLOWED_HOSTS = "['seafile.${vars.domain}']"
       '';
 
       # Optional data directory override
