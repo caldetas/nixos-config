dd9196e2ad51402e593aafc0c38de9d5dd51ea55
caldetas <misterhannes@hotmail.com>
2025-05-21 10:54:18 +0200

    seafile

--- modules/services/seafile2.nix
+++ modules/services/seafile2.nix
@@ -54,10 +54,10 @@ with lib;
           "/" = {
             proxyPass = "http://unix:/run/seahub/gunicorn.sock";
             extraConfig = ''
-              proxy_set_header Host $host;
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Host $server_name;
+              #              proxy_set_header Host $host;
+                            proxy_set_header X-Real-IP $remote_addr;
+                            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                            proxy_set_header X-Forwarded-Host $server_name;
             '';
           };
 
