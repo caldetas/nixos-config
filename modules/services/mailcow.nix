2e9eed80f6130d79c6d3b4e0bd96b9d190145684
caldetas <misterhannes@hotmail.com>
2025-05-22 22:00:47 +0200

    seafile

--- modules/services/mailcow.nix
+++ modules/services/mailcow.nix
@@ -27,6 +27,7 @@ with lib;
         Type = "oneshot";
         RemainAfterExit = true;
         WorkingDirectory = "/home/${vars.user}/git/mailcow-dockerized";
+        ExecStartPre = "/run/current-system/sw/bin/test -d /home/${vars.user}/git/mailcow-dockerized";
         ExecStart = "${pkgs.docker-compose}/bin/docker-compose up -d";
         ExecStop = "${pkgs.docker-compose}/bin/docker-compose down";
         User = vars.user;
@@ -58,5 +59,9 @@ with lib;
   #tar xzf mailcow-config.tar.gz
   #sudo tar xzf mailcow-volumes.tar.gz -C /var/lib/docker/volumes
   #cd mailcow-dockerized && docker-compose up -d
+
+  ### Installation
+  #  Before enabling mailcow clone the repo to /home/caldetas/git/
+  #  git clone https://github.com/mailcow/mailcow-dockerized /home/caldetas/git/mailcow-dockerized
 
 }
