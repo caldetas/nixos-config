c9efb1761c088a3a1c89f507fca14ede24b15e59
caldetas <misterhannes@hotmail.com>
2025-05-24 13:03:58 +0200

    default forwarding nginx

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -1,5 +1,5 @@
 #
-#  hochrheinisches.ch
+#  nginx
 #
 
 { config, lib, pkgs, vars, ... }:
@@ -7,25 +7,23 @@ with lib;
 {
   config = mkIf (config.server.enable) {
     services.nginx = {
-      "_" = {
-        default = true;
-        listen = [
-          { addr = "0.0.0.0"; port = 80; }
-          { addr = "[::]"; port = 80; }
-        ];
-        locations."/" = {
-          return = "302 https://duckduckgo.com";
+      virtualHosts = {
+        "hochrheinisches.ch" = {
+          serverName = "hochrheinisches.ch";
+          forceSSL = true; # Redirect HTTP to HTTPS
+          enableACME = true; # Automatically fetch a Let's Encrypt certificate
+          locations."/" = {
+            return = "301 http://hochrheinisches.clubdesk.ch$request_uri";
+          };
         };
-      };
-      virtualHosts."hochrheinisches.ch" = {
-        serverName = "hochrheinisches.ch";
-        forceSSL = true; # Redirect HTTP to HTTPS
-        enableACME = true; # Automatically fetch a Let's Encrypt certificate
-        locations."/" = {
-          return = "301 http://hochrheinisches.clubdesk.ch$request_uri";
+
+        "_" = {
+          default = true;
+          locations."/" = {
+            return = "302 https://duckduckgo.com";
+          };
         };
       };
     };
   };
-};
 }
