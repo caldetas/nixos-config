b544bc4b982578f8a1d7405f3e2de6e509d9bf32
caldetas <misterhannes@hotmail.com>
2025-06-10 10:16:54 +0200

    immich service vps

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -51,11 +51,10 @@ with lib;
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = ''
-          ${pkgs.bash}/bin/bash -c '
-            cp /etc/immich.env /var/lib/immich/.env &&
-            ${pkgs.docker}/bin/docker compose -f /var/lib/immich/docker-compose.yml up -d
-          '
+          cp /etc/immich.env /var/lib/immich/.env
+          ${pkgs.docker}/bin/docker compose -f /var/lib/immich/docker-compose.yml up -d
         '';
+
         ExecStop = "${pkgs.docker}/bin/docker compose -f /var/lib/immich/docker-compose.yml down";
         WorkingDirectory = "/var/lib/immich";
         Restart = "always";
