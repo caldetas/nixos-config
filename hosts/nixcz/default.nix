c22cd93d8ad882daaaf2be552b806673a3647b1f
caldetas <misterhannes@hotmail.com>
2025-06-16 16:59:24 +0200

    hetzner box

--- hosts/nixcz/default.nix
+++ hosts/nixcz/default.nix
@@ -58,7 +58,7 @@
   mailcow.enable = true;
   seafile.enable = true;
   server.enable = true;
-  immich.enable = ture;
+  immich.enable = true;
 
   networking.networkmanager.enable = true;
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
