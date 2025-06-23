51e57da058af3d949eaee4460297d9d5a311caae
caldetas <misterhannes@hotmail.com>
2025-06-23 11:54:59 +0200

    python seahub

--- modules/services/seafile.nix
+++ modules/services/seafile.nix
@@ -23,76 +23,75 @@ with lib;
           };
         })
       ];
-    }
 
-    # setup after https://wiki.nixos.org/wiki/Seafile
-    # create data folder if not exists
-    # sudo mkdir /mnt/nas/seafile-data && sudo chown -R seafile:seafile /mnt/nas/seafile-data
-    services.seafile = {
-  enable = true;
+      # setup after https://wiki.nixos.org/wiki/Seafile
+      # create data folder if not exists
+      # sudo mkdir /mnt/nas/seafile-data && sudo chown -R seafile:seafile /mnt/nas/seafile-data
+      services.seafile = {
+        enable = true;
 
-  # Set your admin email and initial password
-  adminEmail = "seafile@${vars.domain}";
-  initialAdminPassword = "1234";
+        # Set your admin email and initial password
+        adminEmail = "seafile@${vars.domain}";
+        initialAdminPassword = "1234";
 
-  # External domain (important for web URLs)
-  ccnetSettings.General.SERVICE_URL = "https://seafile.${vars.domain}";
+        # External domain (important for web URLs)
+        ccnetSettings.General.SERVICE_URL = "https://seafile.${vars.domain}";
 
-  # Seafile fileserver config — run behind Nginx using a Unix socket
-  seafileSettings = {
-    fileserver = {
-      max_download_dir_size = 200000; # 200GB
-      max_upload_size = 200000; # 200GB
-      host = "unix:/run/seafile/server.sock";
-      web_token_expire_time = 36000;
+        # Seafile fileserver config — run behind Nginx using a Unix socket
+        seafileSettings = {
+          fileserver = {
+            max_download_dir_size = 200000; # 200GB
+            max_upload_size = 200000; # 200GB
+            host = "unix:/run/seafile/server.sock";
+            web_token_expire_time = 36000;
+          };
+        };
+        seahubExtraConf = ''
+          CSRF_TRUSTED_ORIGINS = ["https://seafile.${vars.domain}"]
+          FILE_SERVER_ROOT =  "https://seafile.${vars.domain}/seafhttp"
+          ALLOWED_HOSTS = ["seafile.${vars.domain}"]
+        '';
+
+        # Optional data directory override
+        dataDir = "/mnt/nas/seafile-data";
+
+        # Garbage collection (e.g. clean up deleted files weekly)
+        gc = {
+          enable = true;
+          dates = [ "Sun 03:00:00" ];
+        };
+      };
+
+      services.nginx = {
+
+        virtualHosts."seafile.${vars.domain}" = {
+          enableACME = true;
+          forceSSL = true;
+
+          locations = {
+            "/" = {
+              proxyPass = "http://unix:/run/seahub/gunicorn.sock";
+              extraConfig = ''
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Host $server_name;
+              '';
+            };
+
+            "/seafhttp" = {
+              proxyPass = "http://unix:/run/seafile/server.sock";
+              extraConfig = ''
+                rewrite ^/seafhttp(.*)$ $1 break;
+                client_max_body_size 0;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_connect_timeout 36000s;
+                proxy_read_timeout 36000s;
+                proxy_send_timeout 36000s;
+                send_timeout 36000s;
+              '';
+            };
+          };
+        };
+      };
     };
-  };
-  seahubExtraConf = ''
-    CSRF_TRUSTED_ORIGINS = ["https://seafile.${vars.domain}"]
-    FILE_SERVER_ROOT =  "https://seafile.${vars.domain}/seafhttp"
-    ALLOWED_HOSTS = ["seafile.${vars.domain}"]
-  '';
-
-  # Optional data directory override
-  dataDir = "/mnt/nas/seafile-data";
-
-  # Garbage collection (e.g. clean up deleted files weekly)
-  gc = {
-    enable = true;
-    dates = [ "Sun 03:00:00" ];
-  };
-};
-
-services.nginx = {
-
-virtualHosts."seafile.${vars.domain}" = {
-enableACME = true;
-forceSSL = true;
-
-locations = {
-"/" = {
-proxyPass = "http://unix:/run/seahub/gunicorn.sock";
-extraConfig = ''
-              proxy_set_header X-Real-IP $remote_addr;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_set_header X-Forwarded-Host $server_name;
-            '';
-};
-
-"/seafhttp" = {
-proxyPass = "http://unix:/run/seafile/server.sock";
-extraConfig = ''
-              rewrite ^/seafhttp(.*)$ $1 break;
-              client_max_body_size 0;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-              proxy_connect_timeout 36000s;
-              proxy_read_timeout 36000s;
-              proxy_send_timeout 36000s;
-              send_timeout 36000s;
-            '';
-};
-};
-};
-};
-};
 }
