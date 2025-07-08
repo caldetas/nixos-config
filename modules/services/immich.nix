88353f1c331501ff05d0e732571e5e5b6feefb3a
caldetas <misterhannes@hotmail.com>
2025-07-08 14:15:54 +0200

    unstable

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -79,6 +79,7 @@ with lib;
       serviceConfig = {
         ExecStart = "${pkgs.docker}/bin/docker compose up -d";
         ExecStop = "${pkgs.docker}/bin/docker compose -f /etc/immich/docker-compose.yml down";
+         RequiresMountsFor = "/mnt/hetzner-box";
         WorkingDirectory = "/var/lib/immich";
         RemainAfterExit = true;
         TimeoutStartSec = 600; #10min
