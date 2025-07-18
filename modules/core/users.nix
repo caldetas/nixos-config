11b62b869f7429da54339a24e857f3a74585caab
caldetas <misterhannes@hotmail.com>
2025-07-18 12:45:36 +0200

    ssh key onsite laptop

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -5,7 +5,8 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "audio" "camera" "networkmanager" "lp" "scanner" "secrets" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDm6jfav0cPBC1nhEkq2lV74xBuwHw70qRFG0uPYZA7O"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDm6jfav0cPBC1nhEkq2lV74xBuwHw70qRFG0uPYZA7O caldetas@libelula"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/fkjSAWbHsV6sos0WfLPgmy9epFhH4asSjQCmEOGIa caldetas@onsite-gnome"
     ];
   };
 }
