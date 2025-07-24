c8395b18393ca470cecbe296fc0a50882fc8ed67
caldetas <misterhannes@hotmail.com>
2025-07-24 14:59:25 +0200

    seafile dockerized

--- modules/services/seafile.nix
+++ modules/services/seafile.nix
@@ -89,12 +89,14 @@ in
           pkgs.writeShellScript "seafile-postsetup" ''
             set -e
             if [ ! -f "${seafilePath}/.setup" ]; then
-                #echo "Waiting to execute seafile-postsetup.."
-                ${pkgs.coreutils}/bin/sleep 5
+                echo "Waiting to execute seafile-postsetup.."
+                ${pkgs.coreutils}/bin/sleep 10
                 ${pkgs.docker}/bin/docker exec seafile sed -i 's/bind = "127.0.0.1:8000"/bind = "0.0.0.0:8000"/' /opt/seafile/conf/gunicorn.conf.py
                 echo "CSRF_TRUSTED_ORIGINS = ['https://seafile.${vars.domain}']" | ${pkgs.coreutils}/bin/tee -a ${seafilePath}/data/seafile/conf/seahub_settings.py > /dev/null
                 ${pkgs.docker}/bin/docker exec seafile /opt/seafile/seafile-server-latest/seahub.sh restart
                 ${pkgs.coreutils}/bin/touch ${seafilePath}/.setup
+            else
+                echo "not executing seafile-postsetup.."
             fi
           '';
       };
