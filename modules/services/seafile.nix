4cbc458d4d9935d32f7794fb2150a87128a71f29
caldetas <misterhannes@hotmail.com>
2025-07-25 10:40:35 +0200

    change to seafile nas

--- modules/services/seafile.nix
+++ modules/services/seafile.nix
@@ -90,9 +90,9 @@ in
           pkgs.writeShellScript "seafile-postsetup" ''
             set -e
             if [ ! -f "${seafilePath}/.setup" ]; then
-                echo "Waiting to execute seafile-postsetup.."
+                echo "Waiting for container to be ready to execute seafile-postsetup.."
                 ${pkgs.coreutils}/bin/sleep 30
-                #${pkgs.docker}/bin/docker exec seafile sed -i 's/bind = "127.0.0.1:8000"/bind = "0.0.0.0:8000"/' /opt/seafile/conf/gunicorn.conf.py
+                ${pkgs.docker}/bin/docker exec seafile sed -i 's/bind = "127.0.0.1:8000"/bind = "0.0.0.0:8000"/' /opt/seafile/conf/gunicorn.conf.py
                 echo "CSRF_TRUSTED_ORIGINS = ['https://seafile.${vars.domain}']" | ${pkgs.coreutils}/bin/tee -a ${seafilePath}/data/seafile/conf/seahub_settings.py > /dev/null
                 ${pkgs.docker}/bin/docker exec seafile /opt/seafile/seafile-server-latest/seahub.sh restart
                 ${pkgs.coreutils}/bin/touch ${seafilePath}/.setup
