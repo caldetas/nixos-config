148deeefc9f920099f08e2083db54da9b41743e8
caldetas <misterhannes@hotmail.com>
2025-10-07 14:19:28 +0200

    revision borg

--- hosts/nixcz/borg.nix
+++ hosts/nixcz/borg.nix
@@ -49,6 +49,7 @@ with lib;
           export PATH=${lib.makeBinPath [ pkgs.docker pkgs.bash pkgs.borgmatic pkgs.borgbackup pkgs.coreutils ]}:$PATH
 
           ./backup.sh
+          mkdir -p /mnt/backup/nixcz/borgmatic
           ${pkgs.borgmatic}/bin/borgmatic init --encryption=repokey-blake2 /mnt/backup/nixcz/borgmatic
           ${pkgs.borgmatic}/bin/borgmatic --verbosity 1 --syslog-verbosity 1
         '';
