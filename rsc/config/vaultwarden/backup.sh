f26f3ac3e563e8291585056260e6bd6d6059a4a9
caldetas <misterhannes@hotmail.com>
2025-12-03 12:05:20 +0100

    vaultwarden backup

--- rsc/config/vaultwarden/backup.sh
+++ rsc/config/vaultwarden/backup.sh
@@ -3,5 +3,4 @@ set -euo pipefail
 #folders are already created in vaultwarden.nix
 
 #copy as tar to hetzner box as tar to preserve permissions
-/bin/mkdir -p /mnt/hetzner-box/backup/nixcz/vaultwarden
-/bin/tar czpf /mnt/hetzner-box/backup/nixcz/vaultwarden/backup-vaultwarden.tar.gz -C /tmp/backup vaultwarden
+tar czpf /mnt/hetzner-box/backup/nixcz/vaultwarden/backup-vaultwarden.tar.gz -C /tmp/backup vaultwarden
