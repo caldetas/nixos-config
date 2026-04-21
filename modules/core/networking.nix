d568717cc4d37ed43fcb4b5f138bb3ea4a4d35bc
caldetas <misterhannes@hotmail.com>
2026-04-21 21:58:49 +0200

    dns revision

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -6,14 +6,17 @@
       enable = true;
       dns = lib.mkForce "none";
     };
-    nameservers = [ "194.169.169.169" ];
+    nameservers = [ "1.1.1.1" ];
   };
 
   services.resolved = {
     enable = true;
-    dnssec = "allow-downgrade";
-    domains = [ "~." ];
-    fallbackDns = [ "1.0.0.1" "1.1.1.1" ];
+    settings.Resolve = {
+      DNS = [ "192.168.178.1" "1.1.1.1" ];
+      DNSSEC = "allow-downgrade";
+      Domains = [ "~." ];
+      FallbackDNS = [ "1.0.0.1" "1.1.1.1" ];
+    };
   };
   services.openssh.enable = true;
 }
