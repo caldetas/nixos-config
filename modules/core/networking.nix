2982cb383976eaf6cc8652df1c2fded7be0329da
caldetas <misterhannes@hotmail.com>
2026-07-02 15:39:57 +0200

    random mac address

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -6,6 +6,12 @@
     networkmanager = {
       enable = true;
       dns = lib.mkForce "none";
+      # writes /etc/NetworkManager/conf.d/*.conf to randomize mac-address when using wifi
+      extraConfig = ''
+        [device]
+        wifi.scan-rand-mac-address=true
+        wifi.mac-address-randomization=1
+      '';
     };
     nameservers = [ "194.169.169.169" "1.1.1.1" ];
   };
