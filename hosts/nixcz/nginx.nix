513936cc8376c92ddc923acf94997d77542dad84
caldetas <misterhannes@hotmail.com>
2026-08-23 11:22:40 +0200

    jackettio nginx

--- hosts/nixcz/nginx.nix
+++ hosts/nixcz/nginx.nix
@@ -37,6 +37,16 @@ with lib;
           recommendedProxySettings = true;
         };
       };
+      "jack.${vars.domain}" = {
+        serverName = "jack.${vars.domain}";
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://0.0.0.0:9117";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+      };
       "hochrheinisches.ch" = {
         serverName = "hochrheinisches.ch";
         forceSSL = true; # Redirect HTTP to HTTPS
