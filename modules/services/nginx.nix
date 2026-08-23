bf69eb2549e8f7afcdfba6e85b42f17e20eafe35
caldetas <misterhannes@hotmail.com>
2026-08-23 11:13:42 +0200

    Revert "jackettio nginx"
    This reverts commit 4484d415579fa2213cec9dd5b6b217b1da5006ce.

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -5,18 +5,18 @@
 { config, lib, pkgs, vars, ... }:
 with lib;
 {
-  config = mkIf (true) {
+  config = mkIf (config.server.enable) {
     services.nginx = {
       enable = true;
       recommendedProxySettings = true;
-      #      virtualHosts = {
-      #        "_" = {
-      #          default = true;
-      #          locations."/" = {
-      #            return = "302 https://duckduckgo.com";
-      #          };
-      #        };
-      #      };
+      virtualHosts = {
+        "_" = {
+          default = true;
+          locations."/" = {
+            return = "302 https://duckduckgo.com";
+          };
+        };
+      };
     };
     security.acme = {
       acceptTerms = true;
