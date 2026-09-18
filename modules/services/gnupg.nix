4e0ed81d066cd956d51ee7309ed7901674538ce4
caldetas <misterhannes@hotmail.com>
2026-09-18 11:17:32 +0200

    gnugpg tweak

--- modules/services/gnupg.nix
+++ modules/services/gnupg.nix
@@ -5,7 +5,7 @@
   services.pcscd.enable = true;
 
   # Ensure D-Bus includes GCR for secure key management
-  services.dbus.packages = [ pkgs.gcr ];
+  services.dbus.packages = [ pkgs.gcr_4 ];
 
   # Enable GnuPG agent with pinentry support
   programs.gnupg.agent = {
@@ -13,13 +13,6 @@
     pinentryPackage = pkgs.pinentry-curses; # Change to pinentry-gtk2 for GUI support
   };
 
-  # Append lines to gpg-agent.conf and gpg.conf only if they don't exist
-  # https://gist.github.com/mjul/56aa1494f65b7f9f38f1aba5b143f579
-  #    systemd.tmpfiles.rules = [
-  #      "L+ /home/${vars.user}/.gnupg/gpg-agent.conf - - - - allow-loopback-pinentry"
-  #      "L+ /home/${vars.user}/.gnupg/gpg.conf - - - - use-agent"
-  #      "L+ /home/${vars.user}/.gnupg/gpg.conf - - - - pinentry-mode loopback"
-  #    ];
 
   home-manager.users.${vars.user} = {
     home.file.".gnupg/gpg.conf" = {
