88f24e826f32e5435c6ce8ef8cb9d3eea42f9824
caldetas <misterhannes@hotmail.com>
2026-09-18 11:37:41 +0200

    Revert "jackett service"
    This reverts commit 932e676bfa2e19c5a79942d61328436e85c1c283.

--- hosts/nixcz/default.nix
+++ hosts/nixcz/default.nix
@@ -57,11 +57,7 @@
   seafile.enable = true;
   server.enable = true;
   immich.enable = true;
-  services.jackett.enable = {
-    enable = true;
-    listenPort = 9117;
-    openFirewall = true;
-  };
+  services.jackett.enable = true;
 
   networking.wireless.enable = lib.mkForce false; #todo delete, wpa was blocking rebuild
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
